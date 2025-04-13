395f104c9a1800f71a07757a7e119c6677c611b1
naufal.ramadhan <muhammad.naufal324@ui.ac.id>
2025-04-14 00:37:44 +0700

    feat: implement caching for ratelimiting with gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 echo "Running migrations..."
 python manage.py migrate
 
+echo "Create cache table..."
+python manage.py createcachetable
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
