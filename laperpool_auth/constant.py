229e76cb676a325884fa7384bed3aefe0b6340c8
naufal.ramadhan <muhammad.naufal324@ui.ac.id>
2025-04-13 20:00:06 +0700

    update home_url constant

--- laperpool_auth/constant.py
+++ laperpool_auth/constant.py
@@ -1,2 +1,2 @@
-#HOME_URL='https://kelompok-58-laperpool-app.pkpl.cs.ui.ac.id'
-HOME_URL='http://localhost:8000/'
+HOME_URL='https://kelompok-58-laperpool-app.pkpl.cs.ui.ac.id'
+# HOME_URL='http://localhost:8000/'
