cc0ca9338877cc1efc7452536ccd244e2d9633b8
naufal.ramadhan <muhammad.naufal324@ui.ac.id>
2025-04-14 23:00:41 +0700

    satinize register

--- my_auth/forms.py
+++ my_auth/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, EmailValidator
+from django.core.exceptions import ValidationError
+from django.utils.html import escape
 from django import forms
 from .models import Customer
 import bcrypt
@@ -23,6 +25,22 @@ class CustomerCreationForm(UserCreationForm):
             'alamat': forms.Textarea(attrs={'rows': 3}),
         }
     
+    def clean_username(self):
+        username = self.cleaned_data.get('username', '').strip()
+        email_validator = EmailValidator(message="Masukkan email yang valid.")
+        email_validator(username)
+        return username
+
+    def clean_alamat(self):
+        alamat = self.cleaned_data.get('alamat', '').strip()
+        return escape(alamat)
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number', '').strip()
+        if not phone_number.isdigit():
+            raise ValidationError("Nomor telepon hanya boleh mengandung angka.")
+        return phone_number
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data["password1"])
