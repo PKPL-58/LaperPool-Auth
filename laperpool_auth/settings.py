0989da2b839770595e02b282d5e6bc828af22a99
naufal.ramadhan <muhammad.naufal324@ui.ac.id>
2025-04-27 11:02:52 +0700

    [TEST] alter database to use sqlite when testing

--- laperpool_auth/settings.py
+++ laperpool_auth/settings.py
@@ -99,6 +99,15 @@ DATABASES = {
     }
 }
 
+import sys
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    }
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
