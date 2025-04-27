c36163e9e338facc7df9100d6ad2fcb6dfe99312
naufal.ramadhan <muhammad.naufal324@ui.ac.id>
2025-04-27 21:37:38 +0700

    [TEST] Add another test for owasp login usecase

--- my_auth/tests.py
+++ my_auth/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 from my_auth.models import Customer
 from laperpool_auth import settings
+from unittest.mock import patch
 
 User = get_user_model()
 
@@ -61,7 +62,37 @@ class LoginTestCase(TestCase):
         self.assertEqual(response.status_code, 302)  # Redirect ke HOME_URL
         self.assertIn(settings.SIMPLE_JWT['AUTH_COOKIE'], response.cookies)  # Pastikan JWT disetel di cookie
 
-    def test_access_control(self):
-        """Test apakah hanya pengguna yang valid dapat login."""
-        "Pengecekan akses kontrol terletak di service laperpool_app dengan decorator @allowed_groups"
-        pass
+    @patch('my_auth.views.logger.info')  # Mock logger.info
+    @patch('my_auth.views.logger.warning')  # Mock logger.warning
+    def test_logging_for_failed_login(self, mock_logger_warning, mock_logger_info):
+        """Test untuk memastikan aktivitas login gagal dicatat."""
+        # Simulasikan login dengan kredensial yang salah
+        response = self.client.post(self.login_url, {'username': 'testuser@example.com', 'password': 'WrongPass123'}, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Username atau password salah.')
+        mock_logger_warning.assert_called_with("Login gagal. Username atau password salah.")
+
+    @patch('my_auth.views.logger.info')  # Mock logger.info
+    def test_logging_for_successful_login(self, mock_logger_info):
+        """Test untuk memastikan aktivitas login berhasil dicatat."""
+        # Simulasikan login dengan kredensial yang benar
+        response = self.client.post(self.login_url, {'username': 'testuser@example.com', 'password': 'SecurePass123'})
+
+        self.assertEqual(response.status_code, 302)  # Redirect ke HOME_URL
+
+        mock_logger_info.assert_any_call("Login berhasil untuk pengguna: testuser@example.com")
+
+    @patch('requests.get')  # Mock requests.get untuk mencegah request sebenarnya
+    def test_ssrf_protection(self, mock_requests_get):
+        """Test untuk memastikan aplikasi tidak rentan terhadap SSRF."""
+        # Mock respons dari requests.get
+        mock_requests_get.return_value.status_code = 400
+
+        malicious_url = "http://127.0.0.1:8000/internal-api"
+        response = self.client.post(self.login_url, {'username': malicious_url, 'password': 'SecurePass123'}, follow=True)
+
+        # Pastikan aplikasi tidak memproses URL berbahaya
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Username hanya boleh mengandung huruf, angka, underscore, @, dan titik.')
+        mock_requests_get.assert_not_called()
